1bdf128ee31f23e434446a035d51e51bc2e9568d
diegozallocco <diegozallocco@hotmail.com>
2023-11-16 11:57:24 -0300

    solving port issue with railway

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3003;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3003, () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log('Server listening at PORT:3003'); // eslint-disable-line no-console
   });
 });
